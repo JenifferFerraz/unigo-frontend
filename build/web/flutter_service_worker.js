aeb58b78735e2922c7d5c0bbca397912228b7ba3
Jeniffer Ferraz Pires <122577117+JenifferFerraz@users.noreply.github.com>
2025-11-09 22:43:35 -0300

    fix: atualizar versão do service worker e hashes dos arquivos de bootstrap

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9f7790e0fd1e6b94f124e034f000cdae
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "c309c8bf9283f9890e81ca42c4fef569",
+"flutter_bootstrap.js": "56dd6d9fd525931cfdd27fbb0849bfb6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
